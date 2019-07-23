e49a90703568915c5250c98949924f4d221aa5d0
hamishgray <hamish.gray@secretescapes.com>
2019-07-23 14:11:49 +0100

    footer structure

---
+++ wp-content/themes/velux/footer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/#footer-php
+ *
+ * @package Primer
+ * @since   1.0.0
+ */
+
+?>
+
+		</div><!-- #content -->
+
+		<footer id="colophon" class="site-footer">
+
+			<div class="site-footer__inner container">
+
+				<div class="row">
+					<div class="col col--4">
+						<?php
+							the_custom_logo();
+						?>
+						<p>
+							Copyright © <?php echo date('Y'); ?> Secret Escapes Ltd.<br />
+							All rights reserved.
+						</p>
+					</div>
+					<div class="col col--8">
+						<?php wp_nav_menu(array(
+							'menu' => 'primary',
+							'orderby' => 'menu_order'
+						)); ?>
+					</div>
+				</div>
+
+
+			</div><!-- .site-footer-inner -->
+
+		</footer><!-- #colophon -->
+
+	</div><!-- #page -->
+
+</body>
+
+</html>
